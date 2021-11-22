f9ddd2e9f0317c689692f94ddc6f29488c5d0d37
Jonasz Schier <schier-jonasz@gmail.com>
2021-11-22 21:45:41 +0100

    project configuration & navbar

---
+++ tappad.web/src/components/NavigationBar/index.vue
@@ -0,0 +1,47 @@
+<template>
+      <v-navigation-drawer
+        v-model="navigation"
+        :mini-variant.sync="mini"
+        permanent
+        color="primary"
+      >
+        <v-list-item class="px-2">
+          <v-list-item-title>tappad</v-list-item-title>
+        </v-list-item>
+        <v-btn
+          icon
+          @click.stop="mini=!mini">
+          <v-icon v-if="this.mini">mdi-chevron-right</v-icon>
+          <v-icon v-else>mdi-chevron-left</v-icon>
+        </v-btn>
+          <v-divider></v-divider>
+          <v-list dense>
+            <v-list-item
+              v-for="page in pages"
+              :key="page.title"
+              link
+            >
+              <v-list-icon>
+                <v-icon>{{ page.icon }}</v-icon>
+              </v-list-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ page.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+      </v-navigation-drawer>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        data: () => ({
+            navigation: true,
+            pages: [
+                {title: 'Home', icon: 'mdi-home-city'}
+            ],
+            mini: true,
+        })
+    };
+</script>
